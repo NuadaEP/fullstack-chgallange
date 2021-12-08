443f9386531816da2d89ae53acc7d4b9207d9531
Bruno Cardoso <bruno@liveonsolutions.com>
2021-12-08 15:47:15 -0300

    adding theme provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${prop => prop.theme.colors};
 `
 
 export default function Home() {
